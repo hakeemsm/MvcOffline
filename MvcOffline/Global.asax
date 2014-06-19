451f4325e2a78ae7878a7ec36df1c3865797ed9a
Hakeem S Mohammad <hakeemosrc@gmail.com>
2014-06-18 23:14:57 -0400

    init

---
+++ MvcOffline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcOffline.Global" Language="C#" %>
---
+++ MvcOffline/Models/DocumentsRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcOffline.Models
+{
+    public class DocumentsRepository
+    {
+        public int AddContent(string userName, string content)
+        {
+            Accounting acc = null;
+            using (DocumentsDataContext dc = new DocumentsDataContext())
+            {
+                acc = new Accounting
+                {
+                    UserName = userName,
+                    Content = content
+                };
+                dc.Accountings.InsertOnSubmit(acc);
+                dc.SubmitChanges();
+            }
+            return acc.Id;
+        }
+
+        public void AddClipping(Accounting accounting, string userName, string clipping)
+        {
+            using (DocumentsDataContext dc = new DocumentsDataContext())
+            {
+                var acc = dc.Accountings.FirstOrDefault(a => a.Id == accounting.Id);
+                acc.Clipping = clipping;
+                acc.UserName = userName;
+                dc.SubmitChanges();
+            }
+        }
+    }
+}
